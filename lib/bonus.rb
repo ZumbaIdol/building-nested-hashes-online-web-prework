d080f18a2a3d0c5427c4357f4a2b09df57171c1d
David Foote <david.a.foote@gmail.com>
2019-06-07 02:30:16 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,9 +21,9 @@ def bonus
   }
 
    epic_tragedy[:montague][:hero][:status] = "dead"
-   puts epic_tragedy
    
    epic_tragedy[:capulet][:heroine[:status] = "dead"
+  
    puts epic_tragedy
 
   
